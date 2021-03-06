e9cdb441c435e2ff3dee246940dc2955bbbd09c6
George <26337405+Spazzinq@users.noreply.github.com>
2021-03-06 11:53:30 -0500

    Fix deps install

--- build/setup/install_deps.sh
+++ build/setup/install_deps.sh
@@ -2,7 +2,7 @@
 apt-get update
 apt-get install -y wget curl gnupg2 lsb-release locales cmake git
 # For building other libraries
-apt-get install -y install jstest-gtk meshlab libprotobuf-dev libprotoc-dev protobuf-compiler ninja-build sip-dev python-empy libtinyxml2-dev libeigen3-dev
+apt-get install -y jstest-gtk meshlab libprotobuf-dev libprotoc-dev protobuf-compiler ninja-build sip-dev python3-empy libtinyxml2-dev libeigen3-dev
 # Install ROS packages
 apt-get install -q -y python3-rosinstall-generator python3-wstool python3-rosinstall libasio-dev libssl-dev rti-connext-dds-5.3.1
 # Download Roborio toolchain
