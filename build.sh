1b363ef77a271099bdb5f3b9429e51015d5fba18
Cole Tucker <cjtucker321@gmail.com>
2021-10-27 00:06:23 -0400

    failed to add libraries

--- build.sh
+++ build.sh
@@ -37,6 +37,8 @@ colcon build --merge-install --metas ${COLCON_META} --cmake-args "--no-warn-unus
 
 echo "Adding build deps to bundle"
 # Get extra deps for export
+
+#TODO Rework this to pull in from lib in cross root
 mkdir extra_libs
 pushd ./extra_libs > /dev/null
 	# get ssl
@@ -50,6 +52,18 @@ pushd ./extra_libs > /dev/null
 
 	# get bison
 	downloadDep "bison_3.0.4-r0.256_cortexa9-vfpv3.ipk"
+
+	# get python 3.5
+	downloadDep "libpython3.5m1.0_3.5.5-r1.0.19_cortexa9-vfpv3.ipk"
+	downloadDep "python3-core_3.5.5-r1.0.51_cortexa9-vfpv3.ipk"
+    downloadDep "python3-dev_3.5.5-r1.0.19_cortexa9-vfpv3.ipk" "gz"
+
+	# get ACL 
+	downloadDep "acl_2.2.52-r0.183_cortexa9-vfpv3.ipk"
+	downloadDep "libacl1_2.2.52-r0.183_cortexa9-vfpv3.ipk"
+
+	# get libattr
+	downloadDep "libattr1_2.4.47-r0.513_cortexa9-vfpv3.ipk"
 
 popd >/dev/null
 
